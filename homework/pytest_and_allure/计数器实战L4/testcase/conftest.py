1693184a4d1660d651744970b2556d473bbbc64c
HerrTTG <herrhaizhenyu@gmail.com>
2024-02-20 14:56:03 +0800

    作业更新L4相关的

--- homework/pytest_and_allure/计数器实战L4/testcase/conftest.py
+++ homework/pytest_and_allure/计数器实战L4/testcase/conftest.py
@@ -16,6 +16,17 @@ def pytest_collection_modifyitems(items: list):
         i._nodeid = i.nodeid.encode("utf-8").decode("unicode_escape")
 
 
+# 给pytest添加自定义命令行参数插件
+def pytest_addoption(parser):
+    # parser
+    mygroup = parser.getgroup("hogwarts")  # group 将下面所有的 option都展示在这个group下。
+    mygroup.addoption("--env",  # 注册一个命令行选项
+                      default='test',  # 参数的默认值
+                      dest='env',  # 存储的变量，为属性命令，可以使用Option对象访问到这个值，暂用不到
+                      help='set your run env'  # 帮助提示 参数的描述信息
+                      )
+
+
 @pytest.fixture(scope='session', autouse=True)
 def envget(request):
     myenv = request.config.getoption("--env", default='test')
