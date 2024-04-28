8b0b46341077ad19c4fb2cb71db9487454732186
HerrTTG <herrhaizhenyu@gmail.com>
2024-04-28 17:48:49 +0800

    学习内容更新

--- 练习和考试题/接口自动化/宠物商店接口自动化实战L4/frame/untils/login.py
+++ 练习和考试题/接口自动化/宠物商店接口自动化实战L4/frame/untils/login.py
@@ -5,16 +5,15 @@ class Loginrequest():
         self.r = requests.request
         self.authname = authname
         self.envinfo = envinfo
+        self.url = envinfo['url']
 
     def login(self, url, userdate):
         return self.r('POST', url, json=userdate, verify=False)
 
     def get_token(self):
         if self.authname == 'admin':
-            url = self.envinfo['admin']['url']
-            res = self.login(url + "/admin/auth/login", self.envinfo['admin']['userinfor'])
-            return url, res.json()["data"]["token"]
+            res = self.login(self.url + "/admin/auth/login", self.envinfo['admin']['userinfor'])
+            return self.url, res.json()["data"]["token"]
         elif self.authname == 'wx':
-            url = self.envinfo['wx']['url']
-            res = self.login(url + "/wx/auth/login", self.envinfo['wx']['userinfor'])
-            return url, res.json()["data"]["token"]
+            res = self.login(self.url + "/wx/auth/login", self.envinfo['wx']['userinfor'])
+            return self.url, res.json()["data"]["token"]
