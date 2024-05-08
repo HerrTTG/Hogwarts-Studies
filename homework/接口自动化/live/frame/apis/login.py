a5b947884faf8ef0bae4e132c9041c1afbc53d35
HerrTTG <herrhaizhenyu@gmail.com>
2024-05-08 11:19:12 +0800

    学习内容更新,login接口的方法描述

--- homework/接口自动化/live/frame/apis/login.py
+++ homework/接口自动化/live/frame/apis/login.py
@@ -1,8 +1,9 @@
 class Login():
     def login(self, method, url, kwargs) -> object:
         """
-        登录接口的实现
-        request为request方法的对象
+        登录接口的实现。
+        只负责描述此接口所做的事。
+        实际动作是对象其父类的send方法去实现，即BaseAPI的send方法。
         """
         r = super().send(method, url, kwargs)
         return r
