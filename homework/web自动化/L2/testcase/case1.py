72c98c65cc31e27e8574320e902576d35a53ef3a
HerrTTG <herrhaizhenyu@gmail.com>
2024-06-03 15:40:43 +0800

    web自动化L2作业提交

---
+++ homework/web自动化/L2/testcase/case1.py
@@ -0,0 +1,100 @@
+import random
+import sys
+
+sys.path.append('..\\..\\L2')
+
+import pytest
+import allure
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from until.untils import Untils
+
+
+@pytest.fixture(scope="class")
+def get_env(request):
+    myenv = request.config.getoption("--env", default='test')
+    envinfo = Untils.envfileload(env=myenv)
+    logging.info(f'用例执行环境信息为:{envinfo}')
+    return envinfo
+
+
+class Test_web():
+    def setup_class(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(3)
+        self.driver.maximize_window()
+
+    def teardown_class(self):
+        self.driver.quit()
+
+    @pytest.mark.parametrize("search", Untils.get_testdata()['datas'])
+    def test_case1(self, search, get_env):
+        id = random.randint(1, 9999)
+        # 动态更新用例标题，可以参与到用例执行中去增加当前用例的标题名
+        allure.dynamic.title(f"用例id:{id}")
+        logging.info(f"用例id:{id}")
+
+        # Test name: 测试人论坛搜索
+        # Step # | name | target | value
+        # 1 | open url
+        self.driver.get(get_env['baseurl'])
+        Untils.save_screenshot(self.driver, id=id, message='open url')
+
+        # 2 | click | 搜索按钮
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.element_to_be_clickable(
+                (By.CSS_SELECTOR, ".d-icon-search"))).click()
+
+        # 3 | click | 高级搜索按钮
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.element_to_be_clickable(
+                (By.CSS_SELECTOR, ".d-icon-sliders-h"))).click()
+
+        # 4 | click | 输入搜索关键字
+        try:
+            WebDriverWait(self.driver, 5).until(
+                expected_conditions.visibility_of_element_located(
+                    (By.XPATH, "//*[text()='高级筛选器']")))
+        except:
+            logging.info(f"用例id:{id} 跳转高级搜索失败")
+            raise AssertionError
+        else:
+            Untils.save_screenshot(self.driver, id=id, message='高级搜索')
+            Untils.save_page_soure(self.driver.page_source, id=id, message='高级搜索')
+
+        self.driver.find_element(By.CSS_SELECTOR, "[aria-label='输入搜索关键字']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "[aria-label='输入搜索关键字']").clear()
+
+        # 5 | type | css=[aria-label='输入搜索关键字']|
+        self.driver.find_element(By.CSS_SELECTOR, "[aria-label='输入搜索关键字']").send_keys(search)
+        logging.info(f"用例id:{id} 输入搜索关键字：{search}")
+        Untils.save_screenshot(self.driver, id=id, message='输入截图')
+
+        # 6 | click | 搜索
+        self.driver.find_element(By.CSS_SELECTOR, "div.search-bar > button ").click()
+
+        # 7 | finds | 标题和高亮
+        ls_t = self.driver.find_elements(By.CSS_SELECTOR, '.topic-title')
+        ls_h = self.driver.find_elements(By.CSS_SELECTOR, '.search-highlight')
+
+        # 8 | assert|
+        try:
+            assert search in ls_t[0].text or search in ls_h[0].text
+        except:
+            print('查询结果失败')
+            Untils.save_screenshot(self.driver, id=id, message='失败结果')
+            Untils.save_page_soure(self.driver.page_source, id=id, message='失败结果')
+            logging.info(f"用例id:{id} 鉴权失败所搜索结果与页面结果不符")
+            logging.info(f"用例id:{id} 第一个标题：{ls_t[0].text}")
+            logging.info(f"用例id:{id} 高亮内容：{ls_h[0].text}")
+            raise AssertionError
+        else:
+            print(ls_t[0].text)
+            logging.info(f"用例id:{id} 鉴权成功")
+            logging.info(f"用例id:{id} 第一个标题：{ls_t[0].text}")
+            logging.info(f"用例id:{id} 高亮内容：{ls_h[0].text}")
+            Untils.save_screenshot(self.driver, id=id, message='成功结果')
+            Untils.save_page_soure(self.driver.page_source, id=id, message='成功结果')
---
+++ homework/web自动化/L2/until/untils.py
@@ -0,0 +1,56 @@
+import allure
+import os
+import time
+import yaml
+
+
+class Untils():
+    @classmethod
+    def get_path(cls):
+        """
+        获取path方法
+        """
+        ## ..\\..\\L2
+        path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        # print(path)
+        return path
+
+    @classmethod
+    def envfileload(cls, env):
+        """
+        根据命令行获取，从不同的配置文件中获取环境信息
+        """
+        myenv = env
+        if myenv == 'test':
+            print('获取并返回环境信息，此条为测试环境')
+            with open(f'{Untils.get_path()}\\config\\envtest.yaml', 'r') as file:
+                envinfo = yaml.safe_load(file)
+        elif myenv == 'dev':
+            print('获取并返回环境信息，此条为开发环境')
+            with open(f'{Untils.get_path()}\\config\\devtest.yaml', 'r') as file:
+                envinfo = yaml.safe_load(file)
+        return envinfo
+
+    @classmethod
+    def get_testdata(cls):
+        """
+        获取测试数据方法，用例中输入的测试数据都由文件形式保存。不体现在用例中
+        """
+        with open(f'{Untils.get_path()}\\datas\\testdata.yaml', 'r', encoding='utf-8') as file:
+            testdata = yaml.safe_load(file)
+        # ['SeleniumA', 'Appium面', '面试']
+        return testdata
+
+    @classmethod
+    def save_screenshot(cls, driver, id=0, message='NA'):
+        file = f'{Untils.get_path()}\\datas\\images\\screenshot_{message}_{id}_{time.strftime("%Y-%m-%d %H-%M-%S", time.gmtime())}.png'
+        driver.save_screenshot(file)
+        allure.attach.file(file, name=message,
+                           attachment_type=allure.attachment_type.PNG)
+
+    @classmethod
+    def save_page_soure(cls, driver, id=0, message='NA'):
+        file = f'{Untils.get_path()}\\datas\\{message}_{id}_{time.strftime("%Y-%m-%d %H-%M-%S", time.gmtime())}.html'
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(driver)
+        allure.attach.file(file, name=message, attachment_type=allure.attachment_type.TEXT)
