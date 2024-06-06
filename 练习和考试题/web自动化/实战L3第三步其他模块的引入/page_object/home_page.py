43522f11a4cf41d2bcf31a7966ef2bcb6f77ab6f
HerrTTG <herrhaizhenyu@gmail.com>
2024-06-06 11:03:37 +0800

    修改注释

--- 练习和考试题/web自动化/实战L3第三步其他模块的引入/page_object/home_page.py
+++ 练习和考试题/web自动化/实战L3第三步其他模块的引入/page_object/home_page.py
@@ -12,12 +12,15 @@ class HomePage(BasePage):
         # 首页：进入商品列表
         # 点击菜单商品管理
         if self.do_find(HomePage.__categorysub_menu).is_displayed():
-            # 点击子菜单商品列表
+            # 如果子菜单已经展示，说明可以直接点击子菜单。
+            # 如果在去点击主菜单后再尝试点子菜单，会因为主菜单点后将子菜单收起而无法点击
             self.do_click(HomePage.__categorysub_menu)
         else:
+            # 先点主菜单
             self.do_click(HomePage.__category)
-            # 点击子菜单商品列表
+            # 再点击子菜单商品列表
             self.do_click(HomePage.__categorysub_menu)
+
         # =>>页面跳转为商品列表管理页面
         Untils.save_screenshot(self.driver, '转为商品列表管理页面')
         from page_object.category_page import CategoryPage
