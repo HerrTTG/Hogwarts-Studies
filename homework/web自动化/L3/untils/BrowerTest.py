565a96c1439fa0537c9d060cbd755d11fa01b645
HerrTTG <herrhaizhenyu@gmail.com>
2024-06-11 09:37:48 +0800

    增加测试用脚本

---
+++ homework/web自动化/L3/untils/BrowerTest.py
@@ -0,0 +1,20 @@
+"""
+测试代码脚本
+"""
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+class TestScript:
+    def setup_class(self):
+        option = Options()
+        option.debugger_address = "localhost:9222"
+        self.driver = webdriver.Chrome(options=option)
+
+    def testscript(self):
+        ele = self.driver.find_element(By.CSS_SELECTOR, "tbody > tr:nth-child(2) > td:nth-child(1) > input")
+        ele.click()
+        time.sleep(3)
