60979ab73f87833c1d75cef488784143a772c4ac
HerrTTG <herrhaizhenyu@gmail.com>
2024-06-11 10:27:03 +0800

    添加测试数据驱动文件化

--- homework/web自动化/L3/testcase/case.py
+++ homework/web自动化/L3/testcase/case.py
@@ -19,6 +19,9 @@ def get_env(request):
     logging.info(f'用例执行环境信息为:{envinfo}')
 
 
+global testdata
+testdata = Untils.get_testdata()
+
 class Test_web():
     def setup_class(self):
         self.home = LoginPage.LoginPage()
@@ -32,14 +35,14 @@ class Test_web():
         """
         self.home.go_home(envinfo)
 
-    @pytest.mark.parametrize('name,id,phone,email', [['hzyetest', '6126361', '18114027157', "123test@123.com"]])
+    @pytest.mark.parametrize('name,id,phone,email', testdata["case1"])
     def testcase1(self, name, id, phone, email):
         result = self.home.login(envinfo).click_addmember().addmember(name, id, phone, email)
         assert result.get_operation_result("add")
         time.sleep(5)
         assert result.ckeck_record(name, phone, email)
 
-    @pytest.mark.parametrize('name,id,phone,email', [['hzyetest', '6126361', '18114027157', "123test@123.com"]])
+    @pytest.mark.parametrize('name,id,phone,email', testdata["case1"])
     def testcase2(self, name, id, phone, email):
         addressbook = self.home.goto_addressbook()
         ele = addressbook.ckeck_record(name, phone, email)
