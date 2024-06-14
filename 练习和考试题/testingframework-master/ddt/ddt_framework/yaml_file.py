61bff4a9f34317dd2add58b32a3f67074ca11b3c
HerrTTG <herrhaizhenyu@gmail.com>
2024-06-14 16:20:16 +0800

    最基础的DDT框架

---
+++ 练习和考试题/testingframework-master/ddt/ddt_framework/yaml_file.py
@@ -0,0 +1,24 @@
+import pytest as pytest
+
+from ddt_framework.yaml_item import YamlItem
+
+
+class YamlFile(pytest.File):
+    def collect(self):
+        # pytest会调用collect，获得每个文件的测试用例
+        # We need a yaml parser, e.g. PyYAML.
+        import yaml
+
+        # 先读取文件的结构 生成为dict
+        raw = yaml.safe_load(self.path.open())
+        if raw:
+            for name, spec in sorted(raw.items()):
+                # 对每个kv结构生成Item对象
+                """
+                如：
+                ok:
+                    sub1: sub2
+                ok这个顶层的key被当做name作为测试用例名，
+                后续的sub1 :sub2 座位测试步骤
+                """
+                yield YamlItem.from_parent(self, name=name, spec=spec)
---
+++ 练习和考试题/testingframework-master/ddt/ddt_framework/yaml_item.py
@@ -0,0 +1,38 @@
+import pytest
+
+from ddt_framework.yaml_exception import YamlException
+
+
+class YamlItem(pytest.Item):
+    def __init__(self, *, spec, **kwargs):
+        """
+        测试用例的定义，需要有测试用例的名字和内容
+        """
+        # 父类将name初始化给self.name
+        super().__init__(**kwargs)
+        # 用例步骤初始化
+        self.spec = spec
+
+    def runtest(self):
+        # 简单的用例执行
+        # 获取用例步骤中的kv。即key为期望值，value为实际值
+        # 做的简单测试就是对比是否一致。
+        for name, value in sorted(self.spec.items()):
+            # Some custom test execution (dumb example follows).
+            if name != value:
+                raise YamlException(self, name, value)
+
+    def repr_failure(self, excinfo):
+        ##异常捕获。
+        """Called when self.runtest() raises an exception."""
+        if isinstance(excinfo.value, YamlException):
+            return "\n".join(
+                [
+                    "usecase execution failed",
+                    "   spec failed: {1!r}: {2!r}".format(*excinfo.value.args),
+                    "   no further details known at this point.",
+                ]
+            )
+
+    def reportinfo(self):
+        return self.path, 0, f"usecase: {self.name}"
