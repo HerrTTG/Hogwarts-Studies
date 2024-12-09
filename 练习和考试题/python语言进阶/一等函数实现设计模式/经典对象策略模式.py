054e5d046d733e83b5f8675aeb70ff38ac6d8499
HerrTTG <herrhaizhenyu@gmail.com>
2024-12-09 10:19:23 +0800

    python学习代码更新

--- 练习和考试题/python语言进阶/一等函数实现设计模式/经典对象策略模式.py
+++ 练习和考试题/python语言进阶/一等函数实现设计模式/经典对象策略模式.py
@@ -5,67 +5,108 @@ from typing import NamedTuple, Optional
 
 
 class Customer(NamedTuple):
+    """
+    客户类：
+    客户类是一个具名元组，用来封装客户对象的具名属性。分别是客户name和积分points
+    """
     name: str
     point: int | float
 
 
 class Item(NamedTuple):
+    """
+    商品类：
+    商品类也是一个具名元组，封装客户购买的商品姓名，数量，和单价。并提供一个缓存特性返回该商品对象的总价。。
+    """
     product: str
     number: int | float
     price: int | float
 
     @property
     @lru_cache
-    def Itemtotal(self):
+    def Itemtotal(self) -> int | float:
         return self.number * self.price
 
 class Order(NamedTuple):
+    """
+    订单类也是一个具名元组，它分别拥有客户属性（来自于客户类的对象）
+    购物车属性（商品类的对象产生的列表）
+    折扣属性，来自于策略类的对象。
+    提供一个汇总购物车总价的特性，返回商品类对象的总价的合计值。
+    提供一个内部方法，计算折扣。
+    实现__str__方法，格式化输出结果，并自动调用内部方法计算折扣
+    """
+
     customer: Customer
     items: Sequence[Item]
     policy: Optional['Policys'] = None
 
     @property
     @lru_cache
-    def total(self):
+    def total(self) -> int | float:
+        # sum函数的第一个位置传入一个生成器推导式来进行迭代，将每个商品的总价进行累加，从0开始
         return sum((item.Itemtotal for item in self.items), start=0)
 
-    def __due(self):
+    def __due(self) -> int | float:
         if self.policy is None:
             return 0
         else:
+            #因为discount中要对购物车中的客户信息，商品信息等进行判断，所以传入self对象。
             return self.total - self.policy.discount(self)
 
     def __str__(self):
         return f"<Order total:{self.total:.2f} Disocunt:{self.__due():.2f}>"
 
 
+class Policys(ABC):
+    """
+    策略类的抽象基类
+    定义策略的行为和参数。
+    """
+
+    @abstractmethod
+    def discount(self, order: Order) -> int | float:
+        pass
+
+
 class PolicyRegister():
+    """
+    策略的注册装饰器。
+    __init__实现参数化装饰器，来控制装饰器是否注册登记这一行为
+    __call__完成注册，并更新类属性。
+    policy为被注册对象
+    此案例中被装饰的是类，所以是所有策略类的类名。
+    """
     policyslist = []
 
     def __init__(self, bool: bool = False):
         self.__bool = bool
 
-    def __call__(self, policy):
+    def __call__(self, policy) -> Policys:
         if self.__bool == True:
             self.__class__.policyslist.append(policy)
         return policy
 
 
-class Policys(ABC):
-
-    @abstractmethod
-    def discount(self, order: Order):
-        pass
-
 
 class BestPolicy(Policys):
-    def discount(self, order: Order):
+    """
+    自动获取最佳折扣值的折扣策略，继承自策略抽象基类。
+    从装饰器类属性中迭代被注册的类名，对其进行构造，并调用折扣方法，最终返回最大值。
+    所以自身不进行注册。避免递归
+    """
+
+    def discount(self, order: Order) -> int | float:
         return max(policy().discount(order) for policy in PolicyRegister.policyslist)
 
 
 @PolicyRegister(True)
 class Pointdiscount(Policys):
-    def discount(self, order: Order):
+    """
+    积分折扣策略类，被装饰。
+    """
+
+    def discount(self, order: Order) -> int | float:
         if order.customer.point > 1000:
             return order.total * 0.05
         else:
@@ -74,7 +115,11 @@ class Pointdiscount(Policys):
 
 @PolicyRegister(True)
 class Itemdiscount(Policys):
-    def discount(self, order: Order):
+    """
+    单一商品商量折扣策略，被装饰。
+    """
+
+    def discount(self, order: Order) -> int | float:
         discount = 0
         for item in order.items:
             if item.number > 20:
@@ -85,7 +130,12 @@ class Itemdiscount(Policys):
 
 @PolicyRegister(True)
 class Orderdiscount(Policys):
-    def discount(self, order: Order):
+    """
+    购物车不同商品数量折扣策略，被装饰。
+    """
+
+    def discount(self, order: Order) -> int | float:
+        #用集合{}推导式，来排除重复的商品名
         if len({item.product for item in order.items}) > 10:
             return order.total * 0.07
         else:
