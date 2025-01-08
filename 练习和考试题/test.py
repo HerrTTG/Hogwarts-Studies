0d82173982326e7dff4c6649885cf6d4eb0ffa88
HerrTTG <herrhaizhenyu@gmail.com>
2025-01-08 12:18:07 +0800

    删除折线图，转为项目代码

--- 练习和考试题/test.py
+++ 练习和考试题/test.py
@@ -1,12 +1,33 @@
-import abc
+from collections import OrderedDict
+
+import csv
+
+f = open("C:\\Users\\96436\\Desktop\\result.csv", 'r')
+fr = csv.reader(f)
+ls = []
+for row in fr:
+    ls.append([word for word in row[0].split(" ") if word != ""])
 
 
-class PerMeta(type):
-    ...
+def csv_to_dict(ls):
+    """
+    将表格转为映射类型，列头key，每一行数据组成的list为value
+    """
+    data = OrderedDict()
+
+    # 统一化表头
+    for colum in ls[0]:
+        data[colum] = list()
+
+    # 从第二行开始依次导入数据到对应的列的列表中
+    row = 1
+    # 判断此行中第一个元素值是否为空
+    while ls[row][0]:
+        keys = (colum for colum in data.keys())
+        for cell in ls[row]:
+            data[next(keys)].append(cell)
+        row += 1
+    return data
 
 
-class Record(abc.ABC, metaclass=PerMeta):
-    ...
-
-
-hhh = Record()
+csv_to_dict(ls)
